2c4111c35a5b2529eb30090a4d8b77a50af47b58
8yunghwi\Byunghwi <gpdldy91@gmail.com>
2017-06-01 21:08:10 +0900

    crud

--- Koggiri/src/main/java/kosta/koggiri/document/controller/Doc_BoardController.java
+++ Koggiri/src/main/java/kosta/koggiri/document/controller/Doc_BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kosta.koggiri.document.domain.Doc_BoardVO;
@@ -40,6 +41,42 @@ public class Doc_BoardController {
 		System.out.println("페이지넘겨버리기~~~");
 		
 		model.addAttribute("list", service.listAll());
+	}
+	
+	@RequestMapping(value="/read", method=RequestMethod.GET)
+	public void read(@RequestParam("f_id")int f_id, Model model)throws Exception{
+		System.out.println("리드페이지로 넘어와버리기~");
+		
+		model.addAttribute(service.read(f_id));//조회된 게시물 jsp로 전달하기위해 모델객체 사용
+		//addAttribute()작업할 때 아무런 이름 없이 데이터를 넣으면 자동으로 클래스의 이름을 소문자로 시작해서 사용.
+		//BoardVO클래스의 객체이므로 boardVO이름
+	}
+	
+	@RequestMapping(value="/remove", method=RequestMethod.POST)
+	public String remove(@RequestParam("f_id")int f_id, RedirectAttributes rttr)throws Exception{
+		
+		service.remove(f_id);
+		
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:/document/listAll";
+		
+	}
+	
+	@RequestMapping(value="/modify", method=RequestMethod.GET)
+	public void modifyGET(int f_id, Model model)throws Exception{
+		
+		model.addAttribute(service.read(f_id));
+	}
+	
+	@RequestMapping(value="/modify", method=RequestMethod.POST)
+	public String modifyPOST(Doc_BoardVO board, RedirectAttributes rttr)throws Exception{
+		
+		service.modify(board);
+		
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:/document/listAll";
 	}
 	
 
