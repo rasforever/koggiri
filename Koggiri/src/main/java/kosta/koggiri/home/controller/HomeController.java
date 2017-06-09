e3ec0e7b0c90de0f2725d21e1e32f34f3dd11cf2
sanghwa <kancho1569@gmail.com>
2017-06-09 12:09:41 +0900

    sadf

--- Koggiri/src/main/java/kosta/koggiri/home/controller/HomeController.java
+++ Koggiri/src/main/java/kosta/koggiri/home/controller/HomeController.java
@@ -1,25 +1,17 @@
 package kosta.koggiri.home.controller;
 
-import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
+
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import kosta.koggiri.admin_emp.domain.EmpVO;
 import kosta.koggiri.admin_emp.service.AdminService;
 
@@ -65,5 +57,6 @@ public class HomeController {
 		return "/main";
 	}
 	
+
 	
 }
