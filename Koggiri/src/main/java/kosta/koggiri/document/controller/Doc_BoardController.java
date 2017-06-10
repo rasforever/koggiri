1e51f5c55c723313096fa08612d9831c664d4129
8yunghwi\Byunghwi <gpdldy91@gmail.com>
2017-06-10 14:15:27 +0900

    d

--- Koggiri/src/main/java/kosta/koggiri/document/controller/Doc_BoardController.java
+++ Koggiri/src/main/java/kosta/koggiri/document/controller/Doc_BoardController.java
@@ -30,119 +30,136 @@ import kosta.koggiri.document.util.MediaUtils;
 @Controller
 @RequestMapping("/document/*")
 public class Doc_BoardController {
-	
+
 	@Inject
 	private Doc_BoardService service;
-	
+
 	@Resource(name = "uploadPath") // 다운로드 경로
 	private String uploadPath;
-	
-	@RequestMapping(value="/register", method=RequestMethod.GET)
-	public void registGET(Doc_BoardVO board, Model model, HttpSession session)throws Exception{
-		
-		String mem_id = (String)session.getAttribute("mem_id");
-		String emp_nm = (String)session.getAttribute("emp_nm");
+
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void registGET(Doc_BoardVO board, Model model, HttpSession session) throws Exception {
+
+		String mem_id = (String) session.getAttribute("mem_id");
+		String emp_nm = (String) session.getAttribute("emp_nm");
 		System.out.println(mem_id);
 		model.addAttribute("mem_id", mem_id);
 		model.addAttribute("emp_nm", emp_nm);
-	
+
 	}
-	
-	@RequestMapping(value="/register", method=RequestMethod.POST)
-	public String registerPOST(Doc_BoardVO board, RedirectAttributes rttr)throws Exception{
-		
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerPOST(Doc_BoardVO board, RedirectAttributes rttr) throws Exception {
+
 		board.getF_emp_id();
 		service.regist(board);
-		
+
 		rttr.addFlashAttribute("msg", "success");
-		
-		//return "/document/success";
+
+		// return "/document/success";
 		return "redirect:/document/list";
 	}
-	
-	@RequestMapping(value="/readPage", method=RequestMethod.GET)
-	public void read(@RequestParam("f_id")int f_id, @ModelAttribute("cri") Doc_SearchCriteria cri, Model model, HttpSession session)throws Exception{
-	
-		String mem_id = (String)session.getAttribute("mem_id");
-		String emp_nm = (String)session.getAttribute("emp_nm");
-		
+
+	@RequestMapping(value = "/readPage", method = RequestMethod.GET)
+	public void read(@RequestParam("f_id") int f_id, @ModelAttribute("cri") Doc_SearchCriteria cri, Model model,
+			HttpSession session) throws Exception {
+
+		String mem_id = (String) session.getAttribute("mem_id");
+		String emp_nm = (String) session.getAttribute("emp_nm");
+
 		model.addAttribute("mem_id", mem_id);
 		model.addAttribute("emp_nm", emp_nm);
 		System.out.println("readPage에서 확인: " + mem_id);
-		
-		model.addAttribute(service.read(f_id));//조회된 게시물 jsp로 전달하기위해 모델객체 사용
-		//addAttribute()작업할 때 아무런 이름 없이 데이터를 넣으면 자동으로 클래스의 이름을 소문자로 시작해서 사용.
-		//BoardVO클래스의 객체이므로 boardVO이름
-	}
-	
 
-	@RequestMapping(value="/removePage", method=RequestMethod.POST)
-	public String remove(@RequestParam("f_id")int f_id, Doc_SearchCriteria cri, RedirectAttributes rttr)throws Exception{
-		
+		model.addAttribute(service.read(f_id));// 조회된 게시물 jsp로 전달하기위해 모델객체 사용
+		// addAttribute()작업할 때 아무런 이름 없이 데이터를 넣으면 자동으로 클래스의 이름을 소문자로 시작해서 사용.
+		// BoardVO클래스의 객체이므로 boardVO이름
+	}
+
+	@RequestMapping(value = "/removePage", method = RequestMethod.POST)
+	public String remove(@RequestParam("f_id") int f_id, Doc_SearchCriteria cri, RedirectAttributes rttr)
+			throws Exception {
+
 		service.remove(f_id);
-		
-		rttr.addAttribute("page", cri.getPage());
-		rttr.addAttribute("perPageNum", cri.getPerPageNum());
-		rttr.addAttribute("searchType", cri.getSearchType());
-		rttr.addAttribute("keyword", cri.getKeyword());
-		
-		
-		rttr.addFlashAttribute("msg", "success");
-		
-		return "redirect:/document/list";
-		
-	}
-	
-	@RequestMapping(value="/modifyPage", method=RequestMethod.GET)
-	public void modifyPagingGET(@RequestParam("f_id")int f_id, @ModelAttribute("cri") Doc_SearchCriteria cri, Model model)throws Exception{
 
-			model.addAttribute(service.read(f_id));
-		
-	
+		rttr.addAttribute("page", cri.getPage());
+		rttr.addAttribute("perPageNum", cri.getPerPageNum());
+		rttr.addAttribute("searchType", cri.getSearchType());
+		rttr.addAttribute("keyword", cri.getKeyword());
+
+		rttr.addFlashAttribute("msg", "success");
+
+		return "redirect:/document/list";
+
 	}
-	
-	@RequestMapping(value="/modifyPage", method=RequestMethod.POST)
-	public String modifyPagingPOST(Doc_BoardVO board, Doc_SearchCriteria cri, RedirectAttributes rttr)throws Exception{
-		
+
+	@RequestMapping(value = "/modifyPage", method = RequestMethod.GET)
+	public void modifyPagingGET(@RequestParam("f_id") int f_id, @ModelAttribute("cri") Doc_SearchCriteria cri,
+			Model model, HttpSession session) throws Exception {
+
+		model.addAttribute(service.read(f_id));
+		String mem_id = (String) session.getAttribute("mem_id");
+		String emp_nm = (String) session.getAttribute("emp_nm");
+		System.out.println(mem_id);
+		model.addAttribute("mem_id", mem_id);
+		model.addAttribute("emp_nm", emp_nm);
+
+	}
+
+	@RequestMapping(value = "/modifyPage", method = RequestMethod.POST)
+	public String modifyPagingPOST(Doc_BoardVO board, Doc_SearchCriteria cri, RedirectAttributes rttr)
+			throws Exception {
+
 		service.modify(board);
-		
+
 		rttr.addAttribute("page", cri.getPage());
 		rttr.addAttribute("perPageNum", cri.getPerPageNum());
 		rttr.addAttribute("searchType", cri.getSearchType());
 		rttr.addAttribute("keyword", cri.getKeyword());
 		rttr.addFlashAttribute("msg", "success");
-		
+
 		return "redirect:/document/list";
 	}
-	
+
 	//
-	
-	@RequestMapping(value="/list", method=RequestMethod.GET)
-	public void listPage(@ModelAttribute("cri") Doc_SearchCriteria cri, Model model)throws Exception{
-		
-		//model.addAttribute("list", service.listCriteria(cri));
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public void listPage(@ModelAttribute("cri") Doc_SearchCriteria cri, Model model, HttpSession session) throws Exception {
+
+		// model.addAttribute("list", service.listCriteria(cri));
 		model.addAttribute("list", service.listSearchCriteria(cri));
-		
+		String mem_id = (String) session.getAttribute("mem_id");
+		String emp_nm = (String) session.getAttribute("emp_nm");
+		System.out.println(mem_id);
+		model.addAttribute("mem_id", mem_id);
+		model.addAttribute("emp_nm", emp_nm);
+
 		Doc_PageMaker pageMaker = new Doc_PageMaker();
 		pageMaker.setCri(cri);
-		//pageMaker.setTotalCount(service.listCountCriteria(cri));
+		// pageMaker.setTotalCount(service.listCountCriteria(cri));
 		pageMaker.setTotalCount(service.listSearchCount(cri));
-		
+
 		model.addAttribute("pageMaker", pageMaker);
-		
+
 	}
-	
-	@RequestMapping(value="/getAttach/{f_id}")
+
+	@RequestMapping(value = "/getAttach/{f_id}")
 	@ResponseBody
-	public List<String>getAttach(@PathVariable("f_id")Integer f_id)throws Exception{
-		
+	public List<String> getAttach(@PathVariable("f_id") Integer f_id) throws Exception {
+
 		return service.getAttach(f_id);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/deleteAllFiles", method = RequestMethod.POST)
-	public ResponseEntity<String> deleteFile(@RequestParam("files[]") String[] files) {// 여러개의 파일 이름을 받을 수 있도록 String[]로 작성.
-
+	public ResponseEntity<String> deleteFile(@RequestParam("files[]") String[] files) {// 여러개의
+																						// 파일
+																						// 이름을
+																						// 받을
+																						// 수
+																						// 있도록
+																						// String[]로
+																						// 작성.
 
 		if (files == null || files.length == 0) {
 			return new ResponseEntity<String>("deleted", HttpStatus.OK);
@@ -165,26 +182,11 @@ public class Doc_BoardController {
 
 		return new ResponseEntity<String>("deleted", HttpStatus.OK);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/deleteFile", method = RequestMethod.POST)
 	public ResponseEntity<String> deleteFile(String fileName) {// 파라미터로 삭제할 파일의
 																// 이름을 받아들임
-
-	
 
 		String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
 
@@ -204,5 +206,4 @@ public class Doc_BoardController {
 
 	}
 
-	
 }
