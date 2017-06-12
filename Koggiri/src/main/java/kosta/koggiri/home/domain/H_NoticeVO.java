218364bdd419963335e1e4117a8b2cab37b40636
Choi yunji <Choi yunji@DESKTOP-701L7EQ>
2017-06-12 21:38:54 +0900

    메인 푸시 ~

---
+++ Koggiri/src/main/java/kosta/koggiri/home/domain/H_NoticeVO.java
@@ -0,0 +1,37 @@
+package kosta.koggiri.home.domain;
+
+public class H_NoticeVO {
+	
+	private Integer N_ID;
+	private String N_TITLE;
+	private String N_DATE;
+	
+	public Integer getN_ID() {
+		return N_ID;
+	}
+
+	public void setN_ID(Integer n_ID) {
+		N_ID = n_ID;
+	}
+
+	public String getN_TITLE() {
+		return N_TITLE;
+	}
+
+	public void setN_TITLE(String n_TITLE) {
+		N_TITLE = n_TITLE;
+	}
+
+	public String getN_DATE() {
+		return N_DATE;
+	}
+
+	public void setN_DATE(String n_DATE) {
+		N_DATE = n_DATE;
+	}
+
+	
+	
+	
+
+}
